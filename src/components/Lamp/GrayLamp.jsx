287e3e80160a38cc79e33cad8a575c976cc5b47b
concefly <98969118coding>
2016-09-01 14:14:03 +0800

    update

--- src/components/Lamp/GrayLamp.jsx
+++ src/components/Lamp/GrayLamp.jsx
@@ -4,12 +4,77 @@ import _ from 'lodash';
 import LampList from './LampList';
 import { Row, Col, Slider, Card, Form, Button, Input } from 'antd';
 import lampsActions from '../../actions/lamps';
-import LampMeta from './LampMeta';
 import styles from './Lamp.module.less';
 
+const sliderMarks = {
+  0: "关",
+  25: "25%",
+  50: "50%",
+  75: "75%",
+  100: "全亮"
+}
 
 const Gutter = () => <div className={styles["gutter"]}></div>;
 
+@Form.create()
+class LampInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false
+    }
+  }
+  handleClickEdit = () => {
+    this.setState({isEditing: true});
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+    const { getFieldsValue } = this.props.form;
+    const { name } = getFieldsValue();
+    this.handleCancel();
+    this.props.onChange({ name });
+  }
+  handleCancel = () => {
+    this.setState({isEditing: false});
+  }
+  render () {
+    const { name, type } = this.props;
+    const { isEditing } = this.state;
+    const { getFieldProps } = this.props.form;
+    const itemSty = {
+      width: '200px'
+    };
+    return (
+      isEditing ?
+      <Form horizontal onSubmit={this.handleSubmit}>
+        <Form.Item label="名称">
+          <Input
+            type="text"
+            placeholder="请输入新名称"
+            style={itemSty}
+            {...getFieldProps('name', {initialValue: name})}
+          />
+        </Form.Item>
+        <Form.Item label="类型">
+          <Input type="text" readOnly value={type} style={itemSty} />
+        </Form.Item>
+        <Button type="primary" htmlType="submit" size="small">确定</Button>
+      </Form> :
+      <div>
+        <p>名称：{name}</p>
+        <p>类型：{type}</p>
+        <p style={{marginTop: 8}}>
+          <Button type="ghost" size="small" onClick={this.handleClickEdit}>编辑</Button>
+        </p>
+      </div>
+    )
+  }
+}
+LampInfo.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
 
 class Lamp extends Component {
   componentDidMount() {
@@ -64,7 +129,7 @@ class Lamp extends Component {
             <div className={styles.panel}>载入中</div> :
             <div className={styles.panel}>
               <Card title="基本信息">
-                <LampMeta
+                <LampInfo
                   name={current.name}
                   type={current.type}
                   onChange={this.handleInfoChange}
