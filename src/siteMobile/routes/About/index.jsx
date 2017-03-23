cfaef7b1c9ad4fd05df52cfe8bbd3182a95a7e7f
concefly <concefly@foxmail.com>
2017-03-23 11:48:46 +0800

    update About

--- src/siteMobile/routes/About/index.jsx
+++ src/siteMobile/routes/About/index.jsx
@@ -2,6 +2,9 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'dva';
 import { Link } from 'dva/router';
 import NavBar from 'antd-mobile/lib/nav-bar';
+import WingBlank from 'antd-mobile/lib/wing-blank';
+import WhiteSpace from 'antd-mobile/lib/white-space';
+import style from './index.module.less';
 
 const Index = ({
   history
@@ -12,7 +15,14 @@ const Index = ({
         leftContent="返回" 
         onLeftClick={() => history.replace('/')}
       >关于</NavBar>
-      <a href="github.com/iLighting">github.com/iLighting</a>
+      <WhiteSpace />
+      <WingBlank>
+        <div className={style.content}>
+          <p>美和舒适的照明</p>
+          <p>华南理工大学 · 光电所</p>
+          <a href="https://github.com/iLighting">https://github.com/iLighting</a>
+        </div>
+      </WingBlank>
     </div>
   );
 }
